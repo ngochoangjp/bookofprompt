6a9e11dadf188ff448c9eb014a9fd1f794161ed7
hoangdn <ngochoangjp@gmail.com>
2025-07-07 09:36:28 +0700

    Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'core/theme/app_theme.dart';
 import 'features/prompt_management/presentation/providers/prompt_provider.dart';
 import 'features/prompt_management/presentation/screens/prompt_manager_screen.dart';
+import 'package:flutter/foundation.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
   runApp(const MyApp());
 }
 
